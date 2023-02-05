4e3e81892c5ec9e2a5d027d776381da0f0d0d7fa
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-05 00:43:19 -0300

    conclusão do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { useState } from 'react';
 import styles  from './App.module.css';
 import poweredImage from './assets/img/powered.png';
-import { levels, calculateImc } from './helpers/imc';
+import { levels, calculateImc, Level } from './helpers/imc';
 import GridItem from './components/gridItem';
+import leftArrowImage from './assets/img/leftarrow.png';
 
 export default function App() {
 
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
 
   const handleCalculatorButton = () => {
     if(heightField && weightField) {
-
+      setToShow(calculateImc(heightField, weightField));
     } else {
       alert('Digite todos os campos');
     }
+  }
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
   }
 
   return (
@@ -34,23 +42,37 @@ export default function App() {
               placeholder="Digite sua altura. Ex: 1.5 (em metros)"
               value={heightField > 0 ? heightField : ''}
               onChange={e => setHeightField(parseFloat(e.target.value))}
+              disabled={toShow ? true : false}
             />
             <input 
               type="number" 
               placeholder="Digite sua peso. Ex: 75.3 (em kg)"
               value={weightField > 0 ? weightField : ''}
               onChange={e => setWeightField(parseFloat(e.target.value))}
+              disabled={toShow ? true : false}
             />
-            <button onClick={handleCalculatorButton}>Calcular</button>
+            <button onClick={handleCalculatorButton} disabled={toShow ? true : false}>
+              Calcular
+            </button>
           </form>
         </div>
 
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, key) =>(
-              <GridItem key={key} item={item} />
-            ))}
-          </div>
+          {!toShow &&           // se eu nao tiver preenchido o formulario eu exibo todos os cards
+            <div className={styles.grid}>
+              {levels.map((item, key) =>(
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          }
+          {toShow &&            // se eu preenchi o formulário e tenho um valor válido eu exibo apenas um dos cards.
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          }
         </div>
       </div>
     </div>
--- src/components/gridItem/index.tsx
+++ src/components/gridItem/index.tsx
@@ -1,4 +1,7 @@
-import { Level } from "../../helpers/imc"
+import { Level } from "../../helpers/imc";
+import styles from './GridItem.module.css';
+import downImage from './../../assets/img/down.png';
+import upImage from './../../assets/img/up.png';
 
 type Props = {
     item: Level
@@ -6,6 +9,25 @@ type Props = {
 
 export default function GridItem({ item }: Props){
     return (
-        <div>...</div>
+        <div className={styles.main} style={{  backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+                <img src={item.icon === "up" ? upImage : downImage} alt="" width="30" />
+            </div>
+            <div className={styles.gridTitle}>
+                {item.title}
+            </div>
+
+            {item.yourImc &&
+                <div className={styles.yourImc}>
+                    Seu IMC é de {item.yourImc} kg/m².
+                </div>
+            }
+            
+            <div className={styles.gridInfo}>
+                <>
+                    IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+        </div>
     )
 }
